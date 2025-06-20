b0ba4152c4e076b6696c9ba0de143fdb139329f7
Pascal Brand <pascal.brand38@gmail.com>
2025-06-20 18:15:17 +0200

    not build step
    Signed-off-by: Pascal Brand <pascal.brand38@gmail.com>

--- src/components.d.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Pascal Brand
-// MIT License
-
-export { default as Neon }  from './components/neon/Neon.astro';
-export { default as Polaroid }  from './components/polaroid/Polaroid.astro';
-export { default as Postit }  from './components/postit/Postit.astro';
-export { default as Garland }  from './components/garland/Garland.astro';
-export { default as Nav }  from './components/nav/Nav.astro';
-export { default as SvgText }  from './components/svgtext/SvgText.astro';
